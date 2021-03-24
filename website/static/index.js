af91650afa437657dd85bd79bd6cfd47563ca1ca
morelir <58606266+morelir@users.noreply.github.com>
2021-03-24 16:08:15 +0200

    Test Complete

--- website/static/index.js
+++ website/static/index.js
@@ -1,2 +1,10 @@
 //this js file is connected to base HTML so it works for any page is loading
 //alert('javascript works!!!')
+function deleteNote(noteId) {
+    fetch("/delete-note", {
+      method: "POST",
+      body: JSON.stringify({ noteId: noteId }),
+    }).then((_res) => {
+      window.location.href = "/";
+    });
+  }

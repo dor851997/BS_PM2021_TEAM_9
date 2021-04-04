cd988c4ad3580088118a93ca19ab5f00072a9e1f
morelir <58606266+morelir@users.noreply.github.com>
2021-04-05 02:25:45 +0300

    BSPM2021T9-131 - answer checking
    checking if the answer is correct continue with the questions else return to the kid page

--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for,session
 from flask_login import login_required, current_user
 
 from . import db
@@ -11,33 +11,82 @@ views = Blueprint('views', __name__)
 @views.route('/kidPage', methods=['GET', 'POST'])
 @login_required
 def kidPage():
-    if request.method == 'POST':
-        if request.form['submit_button'] == 'Start a quiz!':
-            return redirect(url_for('views.question'))
-    cats = QuestionCategory.query.all()
-    return render_template("kidPage.html", user=cats)
+    if current_user.auth=="kid":
+        if request.method == 'POST':
+            if request.form['submit_button'] == 'Start a quiz!':
+                return redirect(url_for('views.question'))
+        cats = QuestionCategory.query.all()
+        return render_template("kidPage.html", user=cats)
+    elif current_user.auth=="editor":
+        flash("No Permission to current user to enter kid page.", category='error')
+        return render_template("editorPage.html", user=current_user)
+    else:
+        flash("No Permission to current user to enter admin page.", category='error')
+        return render_template("adminPage.html", user=current_user)
     
 
 @views.route('/adminPage', methods=['GET', 'POST'])
 @login_required
 def adminPage():
-    
-    return render_template("adminPage.html", user=current_user)
+    if current_user.auth=="admin":
+        return render_template("adminPage.html", user=current_user)
+    elif current_user.auth=="kid":
+        flash("No Permission to current user to enter admin page.", category='error')
+        return render_template("kidPage.html", user=current_user)
+    else:
+        flash("No Permission to current user to enter admin page.", category='error')
+        return render_template("editorPage.html", user=current_user)
     
 @views.route('/editorPage')
 @login_required    
 def editorPage():
-    
-    return render_template("editorPage.html", user=current_user)
+    if current_user.auth=="editor":
+        return render_template("editorPage.html", user=current_user)
+    elif current_user.auth=="kid":
+        flash("No Permission to current user to enter editor page.", category='error')
+        return render_template("kidPage.html", user=current_user)
+    else:
+        flash("No Permission to current user to enter editor page.", category='error')
+        return render_template("adminPage.html", user=current_user)
    
 
-@views.route('/question')
+score=0
+@views.route('/question',methods=['GET', 'POST'])
 @login_required
 def question():
-    # question = Question.query.all()
-    cat = Question.query.filter_by(cat = "Animal").first()
-    print(cat.cat)
+    if request.method == 'POST':
+        if request.form['q_answer']==json.loads(session["questions"][0])['correct']:
+            session["score"]=session["score"]+50
+        else:
+            return redirect(url_for('views.kidPage'))
+
+        session["questions"].pop(0)
+        if len(session["questions"])!=0:
+            question=json.loads(session["questions"][0])
+            return render_template("question.html", user=question,score=session["score"])
+    
     if current_user.auth=="kid":
-        return render_template("question.html", user=cat)
-        
-    return render_template("question.html", user=current_user)
+        questions = Question.query.filter_by(cat = "Animal").all()
+        list=[]
+        for q in questions:
+            list.append(json.dumps(q,default=encoder_question))   
+        session["questions"]=list
+        session["score"]=0
+        question=json.loads(session["questions"][0])
+        return render_template("question.html", user=question,score=session["score"])
+    
+    
+    return render_template("question.html", user=current_user,score=score)
+
+
+
+def encoder_question(question):
+    if isinstance(question,Question):
+        return {'cat':question.cat,
+        'question':question.question,
+        'correct':question.correct,
+        'wrong1':question.wrong1,
+        'wrong2':question.wrong2,
+        'wrong3':question.wrong3
+        }
+    raise TypeError(f'Object {question} is not type of Person.')  

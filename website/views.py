da51e22d62eaaa654c634d1b73c1ced2e172dfa7
morelir <58606266+morelir@users.noreply.github.com>
2021-04-04 11:57:01 +0300

    fixed configuration
    Co-Authored-By: Shaul1Kr <57437693+Shaul1Kr@users.noreply.github.com>
    Co-Authored-By: MatanBarazi <57983441+MatanBarazi@users.noreply.github.com>
    Co-Authored-By: Dor Rozenhak <67059795+dor851997@users.noreply.github.com>

--- website/views.py
+++ website/views.py
@@ -11,46 +11,25 @@ views = Blueprint('views', __name__)
 @views.route('/kidPage', methods=['GET', 'POST'])
 @login_required
 def kidPage():
-    # users=User.query.all()
-    # us=User.query.filter_by(email='asdad@gmail.com').first()
-    # print(us.email)
-    if current_user.auth=="kid":
-        
-        if request.method == 'POST':
-            if request.form['submit_button'] == 'Start a quiz!':
-                return redirect(url_for('views.question'))
-        cats = QuestionCategory.query.all()
-        return render_template("kidPage.html", user=cats)
-    elif current_user.auth=="editor":
-        flash("No Permission to current user to enter kid page.", category='error')
-        return render_template("editorPage.html", user=current_user)
-    else:
-        flash("No Permission to current user to enter admin page.", category='error')
-        return render_template("adminPage.html", user=current_user)
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'Start a quiz!':
+            return redirect(url_for('views.question'))
+    cats = QuestionCategory.query.all()
+    return render_template("kidPage.html", user=cats)
+    
 
-@views.route('/adminPage')
+@views.route('/adminPage', methods=['GET', 'POST'])
 @login_required
 def adminPage():
-    if current_user.auth=="admin":
-        return render_template("adminPage.html", user=current_user)
-    elif current_user.auth=="kid":
-        flash("No Permission to current user to enter admin page.", category='error')
-        return render_template("kidPage.html", user=current_user)
-    else:
-        flash("No Permission to current user to enter admin page.", category='error')
-        return render_template("editorPage.html", user=current_user)
-
+    
+    return render_template("adminPage.html", user=current_user)
+    
 @views.route('/editorPage')
 @login_required    
 def editorPage():
-    if current_user.auth=="editor":
-        return render_template("editorPage.html", user=current_user)
-    elif current_user.auth=="kid":
-        flash("No Permission to current user to enter editor page.", category='error')
-        return render_template("kidPage.html", user=current_user)
-    else:
-        flash("No Permission to current user to enter editor page.", category='error')
-        return render_template("adminPage.html", user=current_user)
+    
+    return render_template("editorPage.html", user=current_user)
+   
 
 @views.route('/question')
 @login_required

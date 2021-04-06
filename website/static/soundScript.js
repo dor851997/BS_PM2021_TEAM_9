28e40da2d09e7798dedfce7b23c579821f9871e6
Dor Rozenhak <67059795+dor851997@users.noreply.github.com>
2021-04-06 21:17:56 +0300

    BSPM2021T9-75 - sound attach to btns
    חיבור צלילים לכפתורים מתאימים

--- website/static/soundScript.js
+++ website/static/soundScript.js
@@ -4,11 +4,12 @@ function playSound(name){
     
     var audio = new Audio("static/sounds/" + name + ".mp3");
     audio.play();
+    setTimeout(function(){ console.log('wait 3000')}, 3000);
     
 }
 
 function clickedSound(){
-    // console.log('func cliked worked...');
+    console.log('func cliked worked...');
     // console.log(window.location.href);
     // var audio = new Audio("static/sounds/clicked.mp3");
     // audio.play();
@@ -16,18 +17,23 @@ function clickedSound(){
 }
 
 function logInSound(){
+    console.log('login');
     playSound("logIn");
 }
 
 
 function logOutSound(){
+    console.log('logout');
     playSound("logOut");
 }
 
 function nextSound(){
+    console.log('next');
     playSound("next");
+    
 }
 
 function wrongSound(){
+    console.log('wrong');
     playSound("wrong");
 }
